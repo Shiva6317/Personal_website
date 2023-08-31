daa8ddc2217e7e3458fed91d1b62185a53411572
shivjeet <shivjeetpaswan86@gmail.com>
2023-08-31 13:55:32 +0530

    initial commit personal website

---
+++ src/data/constants.js
@@ -0,0 +1,309 @@
+import svmiclogo from "../images/svmiclogo.png"
+import reclogo from "../images/recabnlogo.png"
+import piclogo from "../images/peopelsintercollegelogo.png"
+import expresslogo from "../images/expresslogo.png"
+import figmalogo from "../images/figmalogo.png"
+import postmanlogo from "../images/postmanlogo.png"
+import materialuilogo from '../images/materiallogo.png'
+import ekalsutra from '../images/ekalsutralogo.jpg'
+export const Bio = {
+  name: "Shivjeet",
+  roles: [
+    "MERN Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Shiva6317",
+  resume:
+    "https://drive.google.com/file/d/1F7-EDbmlsXwAitU8uagwuYPfxCdsIDWo/view?usp=sharing",
+  linkedin: "",
+  twitter: "",
+  insta: "",
+  facebook: "",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      // {
+      //   name: "Next Js",
+      //   image:
+      //     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      // },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image:materialuilogo,
+      },
+      // {
+      //   name: "Flutter",
+      //   image:
+      //     "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      // },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:expresslogo,
+      },
+      // {
+      //   name: "Graph Ql",
+      //   image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+      // },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      // {
+      //   name: "Postgresql",
+      //   image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      // },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  // {
+  //   title: "Android",
+  //   skills: [
+  //     {
+  //       name: "Java",
+  //       image:
+  //         "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+  //     },
+  //     {
+  //       name: "Kotlin",
+  //       image:
+  //         "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+  //     },
+  //     {
+  //       name: "XML",
+  //       image:
+  //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+  //     },
+  //     {
+  //       name: "Android Studio",
+  //       image:
+  //         "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+  //     },
+  //   ],
+  // },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      // {
+      //   name: "Netlify",
+      //   image:
+      //     "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      // },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:postmanlogo,
+      },
+      // {
+      //   name: "Adobe XD",
+      //   image:
+      //     "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      // },
+      {
+        name: "Figma",
+        image:figmalogo,
+      },
+    ],
+  },
+];
+//////////////////////these are experience section ////////////////////////////
+export const experiences = [
+  {
+    id: 0,
+    img: "https://github.com/Shiva6317/Blog_Application/blob/main/Screenshot%202023-08-31%20122258.png?raw=true",
+    role: "Blog Application",
+    company: "",
+    date: "May 2023 - July 2023",
+    desc: "this is Blog Application in this applicarion I used React,Mongodb,jwt auth,Materialui, girdfs storage,Redux",
+    skills: [
+      "React",
+      "Redux",
+      "JavaScript",
+      "Node Js",
+
+    ],
+    doc: "",
+  },
+
+  {
+    id: 0,
+    img: ekalsutra,
+    role: "Frontend Engineer and API Tester Intern",
+    company: "ekalsutra",
+    date: "Jul 2022 -sep 2022 ",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI and testing api with postman.",
+    skills: [
+      "ReactJS",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "postman",
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: reclogo,
+    school: "Rajkiya Engineering College, Ambedkar Nagar Pin-224122",
+    date: "Oct 2020 - Sep 2024",
+    grade: "7 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Information Tecnology (I.T) and Engineering at Rajkiya engineering College Ambedkar Nagar.  I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.  I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Information Technology and Engineering",
+  },
+  {
+    id: 1,
+    img: piclogo,
+    school: "Peoples Inter College Domariyaganj",
+    date: "Apr 2019 - Apr 2020",
+    grade: "79%",
+    desc: "I completed my class 12 intermediate education at Peoples Inter College ",
+    degree: "U.P Borad Intermediate ,Math and Science",
+  },
+  {
+    id: 2,
+    img: svmiclogo,
+    school: "Sarsvati Vidya Mandir inter College",
+    date: "Apr 2017 - Apr 2018",
+    grade: "84%",
+    desc: "I completed my class 10 education at Sarsvati vidya mandir inter college",
+    degree: "U.P Borad Highschool ,Math and Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Blog Application",
+    date: "Jun 2023 - Apr 2023",
+    description:
+      "MyBlogSpace is a user-friendly and feature-rich blogging application that empowers users to share their thoughts, experiences, and stories with the world. With a simple and intuitive interface, it offers a seamless experience for both new and experienced bloggers.",
+    image:
+      "https://github.com/Shiva6317/Blog_Application/blob/main/Screenshot%202023-08-31%20122258.png?raw=true",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+    ],
+    category: "web app",
+    github: "https://github.com/Shiva6317/Blog_Application",
+    webapp: "",
+  },
+  {
+    id: 0,
+    title: "Personal Website",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Developed a full-stack web ",
+    image:
+      "https://raw.githubusercontent.com/Shiva6317/Personal_website/main/Screenshot%202023-08-31%20120834.png",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    github: "https://github.com/Shiva6317/Personal_website",
+    webapp: "",
+    
+  },
+ 
+  {
+    id: 5,
+    title: "Todo Web App",
+    date: "Jun 2021",
+    description:
+      " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+    image:
+      "https://raw.githubusercontent.com/Shiva6317/Personal_website/main/Screenshot%202023-08-31%20120834.png",
+    tags: ["React Js", "Local Storage", "Jwt Auth", "Node JS"],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2019, text: "Started my journey" },
+  { year: 2020, text: "Worked as a freelance developer" },
+  { year: 2021, text: "Founded JavaScript Mastery" },
+  { year: 2023, text: "Shared my projects with the world" },
+  
+];
